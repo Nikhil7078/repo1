c5bfae7be3fb15e5c4dd5ccd8a039f18a54d4aae
2018167@iiitdmj.ac.in <you@example.com>
2022-04-08 16:41:13 +0530

    Webscraping

---
+++ scorecards.js
@@ -0,0 +1,14 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+function getInfromScorecard(url)
+{
+    console.log("from scorecards.js",url);
+    // request(url,cb);
+}
+
+module.exports = {
+    gifs:gitIntoFormScorecard,
+};
+
+
