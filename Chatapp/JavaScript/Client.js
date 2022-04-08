50daf2ae0e80b4c5b29a670d16ac6a68d3eb2b18
2018167@iiitdmj.ac.in <you@example.com>
2022-04-08 19:46:50 +0530

    Chatapp

---
+++ Chatapp/JavaScript/Client.js
@@ -0,0 +1,34 @@
+const socket = io('http://localhost:8000');  
+ const form = document.getElementById("sending-system");
+ const messageInput = document.getElementById("input");
+ const messagecontainer =  document.querySelector(".container");
+
+var audio = new Audio('Sound/Ting.mp3');
+
+ const append = (message,position)=>{
+   const messageElement = document.createElement('div');
+   messageElement.innerText = message;
+   messageElement.classList.add(position);
+   messagecontainer.append(messageElement);
+   audio.play();
+ }
+
+ 
+ const Name = prompt("Enter your name to join");
+ socket.emit('new-user-joined',Name);
+
+ socket.on('user-joined',Name =>{
+ append(`${Name} joined the chat`,'messageleft');
+ });
+ 
+form.addEventListener('submit',(e)=>
+{
+      e.preventDefault(); 
+      const message = messageInput.value;
+      append(message,'messageright');
+      socket.emit('send',message); 
+      messageInput.value = '';
+ });
+ socket.on('Receive',data =>{
+    append(data.message,'messageleft')
+ });
---
+++ Chatapp/index.js
@@ -0,0 +1,19 @@
+// this is our NodeServer which will handle socket.io connection 
+const io = require('socket.io')(8000,{
+    cors: {
+        origin:"*",
+    }
+});
+const user = {};
+
+io.on('connection',socket =>{
+    socket.on('new-user-joined',name =>{
+        // console.log(name);
+        user[socket.id] = name;
+        socket.broadcast.emit('user-joined',name);
+    });
+    socket.on('send',message=>{
+        socket.broadcast.emit('Receive',{message:message,name:user[socket.id]})
+    })
+});
+//io.on('connection',)
